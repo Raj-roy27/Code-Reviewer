5e660d842f10c02f27bc4939a40f806aac6a8287
Raj Roy <royr938559@gmail.com>
2026-03-15 19:46:46 +0530

    Feature: Enhance with Markdown, cors & Finalize project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "prismjs/themes/prism-tomorrow.css";
+import ReactSimpleEditor from "react-simple-code-editor";
+import prism from "prismjs";
+import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+import axios from "axios";
+
+const Editor = ReactSimpleEditor.default || ReactSimpleEditor;
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [code, setCode] = useState(`function sum(){\n  return 1+1\n}`);
 
-export default App
+  const [review, setReview] = useState(``);
+
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  const reviewCode = async () => {
+    const response = await axios.post("http://localhost:3000/ai/get-review", {
+      code,
+    });
+
+    setReview(response.data);
+  };
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript, "javascript")
+              }
+              padding={15}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 15,
+                borderRadius: "0.8rem",
+                height: "100%",
+                width: "100%",
+              }}
+            ></Editor>
+          </div>
+          <div onClick={reviewCode} className="review">
+            Review
+          </div>
+        </div>
+        <div className="right">
+          <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
